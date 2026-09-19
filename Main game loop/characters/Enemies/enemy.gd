fb4f90aab2bca9330b83d894f0f619f843d8e6ec
hashem <halfai9234@gmail.com>
2026-09-19 16:24:18 +0300

    I cant even fix enemy rotation, what a failure for today

--- Main game loop/characters/Enemies/enemy.gd
+++ Main game loop/characters/Enemies/enemy.gd
@@ -7,7 +7,7 @@ var enemy_detected_flag = false
 @export var hp = 15
 @export var extra_smash_damge = 14
 var rotation_friction = 1.15
-var rotaion_velocity:float
+var rotation_velocity:float
 @onready var dmg_sound: AudioStreamPlayer3D = $AudioStreamPlayer3D
 var file_controls = file_control.new()
 var savedata:Dictionary
@@ -20,36 +20,35 @@ func _ready() -> void:
 	dmg_sound.volume_linear = savedata["settings"]["sounds"][1]
 
 func _process(delta: float) -> void:
-	if enemy_detected_flag == true:
-		var desireable_rotation = atan2((player.position.z-position.z),(player.position.x-position.x))
+	if enemy_detected_flag == false:
+		if rotation_speed == null:
+			rotation_speed = 0.5
+		print(name+str(rotation_degrees.y))
+		var desireable_rotation = atan2((player.position.x-position.x),(player.position.z-position.z))-PI/2
 		if absf(desireable_rotation-rotation.y)<0.05:
 			pass
 		elif desireable_rotation>rotation.y:
-			if desireable_rotation>rotation_degrees.y:
-				rotaion_velocity += -1*delta*rotation_speed
-				
+			if desireable_rotation-PI>rotation.y:
+				rotation_velocity += -1*delta*rotation_speed
 			else:
-				rotaion_velocity += 1*delta*rotation_speed
+				rotation_velocity += 1*delta*rotation_speed
 		else:
-			if desireable_rotation<rotation_degrees.y:
-				rotaion_velocity += 1*delta*rotation_speed
+			if desireable_rotation+PI<rotation.y:
+				rotation_velocity += 1*delta*rotation_speed
 			else:
-				rotaion_velocity += -1*delta*rotation_speed
-		rotaion_velocity /= rotation_friction
-		rotaion_velocity = clampf(rotaion_velocity,min_rotation,max_rotation)
-		rotation.y += rotaion_velocity
-
-func _on_hitbox_area_entered(area: Area2D) -> void:
-	enemy_detected_flag = true
+				rotation_velocity += -1*delta*rotation_speed
+		rotation_velocity /= rotation_friction
+		rotation_velocity = clampf(rotation_velocity,min_rotation,max_rotation)
+		rotation.y += rotation_velocity
+		
+func _on_hitbox_area_entered(area: Area3D) -> void:
 	print(name+",took damge from:"+area.name)
 	hp -= 1 
 	dmg_sound.play()
 	if area.name == "Smash":
 		hp -= extra_smash_damge
 	if hp <= 0:
-		kill.emit()
 		queue_free()
-	
 
 
 func _on_timer_timeout() -> void:
@@ -66,10 +65,3 @@ func shoot():
 	var bullet = BULLET.instantiate()
 	get_tree().root.add_child(bullet)
 	bullet.start(position,rotation)
-
-
-func _on_seeing_radius_body_entered(body: Node2D) -> void:
-	enemy_detected_flag = true
-
-func _on_seeing_radius_body_exited(body: Node2D) -> void:
-	enemy_detected_flag = false
--- Main game loop/characters/Player/Player bullets/Bullet.gd
+++ Main game loop/characters/Player/Player bullets/Bullet.gd
@@ -4,7 +4,7 @@ extends Area3D
 @export var damage = 1
 var line_of_fire
 func start(pos,the_rotation,volume):
-	print("bullet spawned")
+	print( name+" spawned")
 	position = pos
 	line_of_fire = the_rotation
 	line_of_fire -= the_rotation
@@ -14,18 +14,17 @@ func start(pos,the_rotation,volume):
 func _process(delta):
 	if line_of_fire == null:
 		return
-	print(rad_to_deg(line_of_fire))
 	position += Vector3(1,0,0).rotated(Vector3(0,1,0),line_of_fire)*speed*delta
 func _on_enemy(area: Area3D) -> void:
 	if area.collision_layer == 32:
-		print("player,"+name+",despawned from hitting:"+area.name)
+		print("player "+name+",despawned from hitting:"+area.name)
 		queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	print("player,"+name+",despawned from going off screen")
+	print("player "+name+", despawned from going off screen")
 	queue_free()
 
 
 func _on_body_entered(body: Node3D) -> void:
-	print("player,"+name+",despawned from hitting:"+body.name)
+	print("player "+name+", despawned from hitting:"+body.name)
 	queue_free()
